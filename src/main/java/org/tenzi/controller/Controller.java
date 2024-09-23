588833ffc8981a29733b783897c84da4ebafa1e6
tenziTGM <tenzi@student.tgm.ac.at>
2024-09-23 14:05:25 +0200

    Added Project Structure and Diagram

--- src/main/java/org/tenzi/controller/Controller.java
+++ src/main/java/org/tenzi/controller/Controller.java
@@ -1,8 +1,8 @@
-package org.tenzi;
+package org.tenzi.controller;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Controller {
     public static void main(String[] args) {
         // Press Alt+Eingabe with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
